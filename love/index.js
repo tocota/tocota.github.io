9db3aea9cc180622e06533b0e5df8690d73353b2
Wang TianWen <wangtianwen90@163.com>
2021-10-30 22:08:29 +0800

    Site updated: 2021-10-30 22:08:27

--- love/index.js
+++ love/index.js
@@ -3,7 +3,7 @@ var htmer_time_time = null;
 
 function setTime() {
   var create_time = Math.round(
-    new Date(Date.UTC(2018, 10, 26, 6, 45, 0)).getTime() / 1000
+    new Date(Date.UTC(2021, 10, 02, 00, 00, 00)).getTime() / 1000
   );
   var timestamp = Math.round(
     (new Date().getTime() + 8 * 60 * 60 * 1000) / 1000
